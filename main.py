005bc8e7ddbc397ca8db45ee1b5a3500e835a75e
Annie Szorkin <antonsorkin3@gmail.com>
2025-06-01 13:13:26 -0400

    Added json data logging.

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from openai.types.chat import (
     chat_completion_message,
 )
 
+import json
+import time
+
 from martian_apart_hack_sdk import judge_specs, martian_client, utils
 from martian_apart_hack_sdk.models import llm_models
 
@@ -44,12 +47,15 @@ def init_judged_convo(chat_request_text = "What is a good Chinese restaurant in 
     return judged_convo, completion_request
 
 
-def evaluate_judge(client, rubric_judge_spec, completion_request, chat_completion_response):
+def evaluate_judge(client: martian_client.MartianClient, rubric_judge_spec, completion_request, chat_completion_response):
     return client.judges.evaluate_using_judge_spec(
         rubric_judge_spec.to_dict(),
         completion_request=completion_request,
         completion_response=chat_completion_response,
     )
+
+def timestamp() -> str:
+    return time.strftime("%Y-%m-%d_%H:%M:%S")
 
 
 def main():
@@ -72,13 +78,33 @@ def main():
         judge_result = evaluate_judge(client, judge_spec, completion_request, judged_convo)
         print(f"Judge result: {judge_result}")
 
-        if rubric not in responses:
-            responses[rubric] = []
-        responses[rubric].append(judge_result)
+        normalized_score = (judge_result.score - judge_spec.min_score) / (judge_spec.max_score - judge_spec.min_score)
+
+        responses[rubric] = {}
+        responses[rubric]["judge_spec"] = judge_spec.to_dict()
+        responses[rubric]["request"] = chat_request_text
+        responses[rubric]["response"] = chat_response_text
+        responses[rubric]["raw_score"] = judge_result.score
+        responses[rubric]["reason"] = judge_result.reason
+        responses[rubric]["norm_score"] = float(normalized_score)
+
+
+        print(f"Eval score {judge_result.score}")
+
+        with open(f"./results/{timestamp()}_main_rubric_{rubric}.json", "w") as out_f:
+            json.dump(responses[rubric], out_f, indent=2)
 
     print(responses)
-        
 
+    with open(f"./results/{timestamp()}_main_responses.json", "w") as out_f:
+        json.dump(responses, out_f, indent=2)
+
+    summarized_results = {rubric:responses[rubric]["norm_score"] for rubric in responses}
+    summarized_results["_average"] = sum(summarized_results.values()) / len(summarized_results)
+
+    with open(f"./results/{timestamp()}_main_summary_responses.json", "w") as out_f:
+        json.dump(summarized_results, out_f, indent=2)
+    
 
 if __name__ == "__main__":
     main()
